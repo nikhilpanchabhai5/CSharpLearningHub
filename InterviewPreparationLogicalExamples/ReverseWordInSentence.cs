b627d25af68c3e349f3233496e615862cbda8ea4
nikhilpanchabhai5 <nikhilpanchabhai@yahoo.com>
2026-02-27 14:16:40 +0530

    Initial commit

---
+++ InterviewPreparationLogicalExamples/ReverseWordInSentence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparationLogicalExamples
+{
+    internal class ReverseWordInSentence
+    {
+        public ReverseWordInSentence() 
+        {
+            Console.WriteLine("Enter your sentence:");
+            string inputString = Console.ReadLine();
+            string[] words = inputString.Split(' ');
+            StringBuilder stringBuilder = new StringBuilder();            
+            for(int i =words.Length-1;  i >= 0; i--)
+            {
+                if (stringBuilder.Length > 0)
+                    stringBuilder.Append(' ');
+                stringBuilder.Append(words[i]);
+            }                
+            Console.WriteLine(stringBuilder.ToString());
+
+            var reversedWordSentence = inputString.Split(' ')
+                .Select(word => word.Reverse().ToArray());
+
+        }
+    }
+}
---
+++ InterviewPreparationLogicalExamples/StringIsPalindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparationLogicalExamples
+{
+    internal class StringIsPalindrome
+    {
+        public StringIsPalindrome() { 
+            //Console.WriteLine("Enter string:");
+            string inputString = "nitin";
+            inputString = inputString.ToLower().Replace(" ", "");
+            Console.WriteLine(inputString);
+            int i = 0;
+            int j = inputString.Length - 1;
+            bool isPalindrome = true;
+            while (i < j)
+            {
+                if (inputString[i] != inputString[j])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                i++;
+            }
+            Console.WriteLine(inputString);
+            if (isPalindrome)
+                Console.WriteLine("String is a palindrome");
+            else
+                Console.WriteLine("String is not palindrome");
+
+        }
+    }
+}
